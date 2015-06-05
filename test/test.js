80b7f1ef32c8c817091fb492ad60e767e3f290c8
古映杰 <yj_gu@Ctrip.com>
2015-06-05 17:17:47 +0800

    save

--- test/test.js
+++ test/test.js
@@ -2,5 +2,6 @@ import Tree from '../lib/tree'
 var tree = new Tree(process.cwd())
 
 tree.readdir().then(() => {
-	return tree.getProgeny('node_modules/express/lib').then((result) => tree.saveTo('./test/readdir.json'))
+	return tree.getProgeny('node_modules/express/lib')
+	.then((subTree) => subTree.saveTo('./test/readdir.json'))
 }).catch((err) => console.log(err))
