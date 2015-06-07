db2842853acd7a8f046af75d8ea2ea2f1b5dc1b2
Jade <guyingjie129@163.com>
2015-06-07 20:11:49 +0800

    add s6-tree

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 import Tree from '../lib/tree'
+import path from 'path'
 var tree = new Tree(process.cwd())
 
-tree.readdir().then(() => {
-	return tree.getProgeny('node_modules/express/lib')
-	.then((subTree) => subTree.saveTo('./test/readdir.json'))
-}).catch((err) => console.log(err))
+// tree.readdir().then(() => {
+// 	return tree.getProgeny('node_modules/express/lib/router/index.js')
+// 	.then((index) => {
+// 		return tree.getProgeny('test')
+// 		.then((test) => {
+// 			return test.addFile('test_addFile.json', tree.json())
+// 		})
+// 	})
+// }).catch((err) => console.log(err))
+
+tree.removeDir('test/tree.json')
